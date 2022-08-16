2047c32159f1ce533e736905c688efc995a9eaea
ThulioAugusto <thulio.aaa@hotmail.com>
2022-08-15 22:37:59 -0300

    ChessException and ChessPosition

---
+++ src/Chess/ChessException.java
@@ -0,0 +1,9 @@
+package Chess;
+
+public class ChessException extends RuntimeException {
+    public static final long serialVersionUID = 1L;
+    public ChessException(String msg){
+        super(msg);
+    }
+
+}
